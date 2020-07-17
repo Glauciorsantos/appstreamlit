a43fb0d74d690b1e075f9ef75fdf333ff4c230ab
Glauciorsantos <68239593+Glauciorsantos@users.noreply.github.com>
2020-07-17 09:17:07 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 mkdir -p ~/.streamlit/
 
+eco  " \
+[geral] \ n \
+email = \ " glauciorsantos@hotmail.com\" \ n \
+"  >  ~ /.streamlit/credentials.toml
+
+
 echo "\
 [server]\n\
 headless = true\n\
